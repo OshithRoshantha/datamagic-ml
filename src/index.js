8d167e556283b69bee928ef53f784407b4dd3357
Oshith Roshantha <oedirisuriya@gmail.com>
2025-02-15 23:24:47 +0530

    add MissingValues to index for improved data preprocessing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const MinMaxScaler = require('./normalization');
 const StandardScaler = require('./standardization');
 const OneHotEncoder = require('./oneHotEncoding');
+const MissingValues = require('./missingValues');
 
 module.exports = {
     MinMaxScaler,
     StandardScaler,
-    OneHotEncoder
+    OneHotEncoder,
+    MissingValues
 };
